4835498ce287278257a702f1fa701dc0454b4f60
nicorapelas <nicorapelas@gmail.com>
2024-02-02 16:58:53 +0200

    added createCommunity action

--- context/CommunityContext.js
+++ context/CommunityContext.js
@@ -10,6 +10,8 @@ const CommunityReducer = (state, action) => {
       return { ...state, error: action.payload, loading: false }
     case 'FETCH_COMMUNITY':
       return { ...state, community: action.payload, loading: false }
+    case 'CREATE_COMMUNITY':
+      return { ...state, community: action.payload, loading: false }
     default:
       return state
   }
@@ -19,8 +21,27 @@ const CommunityReducer = (state, action) => {
 const fetchCommunity = (dispatch) => async () => {
   dispatch({ type: 'LOADING' })
   try {
-    const response = await ngrokApi.get('/community/fetch-community')
+    const response = await ngrokApi.get('/community/fetch')
     dispatch({ type: 'FETCH_COMMUNITY', payload: response.data })
+  } catch (error) {
+    dispatch({
+      type: 'SET_ERROR',
+      payload: error,
+    })
+  }
+}
+
+const createCommunity = (dispatch) => async () => {
+  dispatch({ type: 'LOADING' })
+  try {
+    const response = await ngrokApi.post('/community/create')
+    console.log(`response:`, response)
+    if (response.data.error) {
+      dispatch({ type: 'SET_ERROR', payload: response.data.error })
+      return
+    }
+    dispatch({ type: 'CREATE_COMMUNITY', payload: response.data })
+    return
   } catch (error) {
     dispatch({
       type: 'SET_ERROR',
@@ -38,6 +59,7 @@ export const { Provider, Context } = createDataContext(
   {
     setError,
     fetchCommunity,
+    createCommunity,
   },
   {
     loading: false,
